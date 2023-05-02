d02940bdbfeee62d6705ea843193d8ed3418fa92
Dobby <apple@Dobbyde-MacBook-Pro.local>
2023-05-01 22:34:56 -0400

    add auth in client

---
+++ client/src/utils/auth.js
@@ -0,0 +1,35 @@
+import decoded from "jwt-decode";
+
+class AuthService {
+  getToken() {
+    return localStorage.getItem("token");
+  }
+  isTokenExpired(token) {
+    try {
+      const decoded = decoded(token);
+      if (decoded.exp < Date.now() / 1000) {
+        return true;
+      } else return false;
+    } catch (err) {
+      return false;
+    }
+  }
+  loggedIn() {
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token);
+  }
+
+  getProfile() {
+    return decoded(this.getToken());
+  }
+  loggedIn(idToken) {
+    localStorage.setItem("id_token", idToken);
+    window.location.assign("/");
+  }
+  logout() {
+    localStorage.removeItem("id_token");
+    window.location.assign("/");
+  }
+}
+
+export default new AuthService();
